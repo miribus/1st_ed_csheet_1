8e162e4b10a8c8e30cabae05cd9d0c065bcffdcc
todd s <tsirois76@gmail.com>
2019-12-12 20:07:10 -0500

    now 'name' object just passes back and forth implemented 'define abilities' to detail ability bonuses for most stuff
    class detail next

--- character.py
+++ character.py
@@ -1,4 +1,4 @@
-import dice, saving_throws, char_races, char_classes, social_class
+import dice, saving_throws, char_races, char_classes, social_class, base_ability_detail
 
 class playerSheet:
     def __init__(self, charname):
@@ -18,6 +18,14 @@ class playerSheet:
                                "CON": dice.ability_roller(6, 5),
                                "CHA": dice.ability_roller(6, 5),
                                "CMS": dice.ability_roller(6, 3)}
+        self.char_define_abilities = {}
+        self.char_define_abilities["STR"] = {}
+        self.char_define_abilities["INT"] = {}
+        self.char_define_abilities["WIS"] = {}
+        self.char_define_abilities["DEX"] = {}
+        self.char_define_abilities["CON"] = {}
+        self.char_define_abilities["CHA"] = {}
+        self.char_define_abilities["CMS"] = {}
         self.char_HP = 0
         self.char_AC = {"Base AC:":10, "Shield AC:":0, "Rear AC:":0}
         self.char_saves = {}
@@ -63,9 +71,24 @@ if decision.isalpha():
             name, result = char_classes.race_classes(name)
         name = char_classes.class_saving_throws(name)
         name = char_races.base_bonuses(name)
+        # name.char_define_abilities = {}
+        name = base_ability_detail.define_abilities(name)
 print(name.char_race)
 print(name.char_social_class)
-print(name.char_abilities)
+print(name.char_abilities["STR"])
+print(name.char_define_abilities["STR"])
+print(name.char_abilities["INT"])
+print(name.char_define_abilities["INT"])
+print(name.char_abilities["WIS"])
+print(name.char_define_abilities["WIS"])
+print(name.char_abilities["DEX"])
+print(name.char_define_abilities["DEX"])
+print(name.char_abilities["CON"])
+print(name.char_define_abilities["CON"])
+print(name.char_abilities["CHA"])
+print(name.char_define_abilities["CHA"])
+print(name.char_abilities["CMS"])
+print(name.char_define_abilities["CMS"])
 print(name.char_class)
 print(name.char_saves)
 #print(name.char_race_abilities)
